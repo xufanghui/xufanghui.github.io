2562c6a4a3a29ea9ce26f71ca7fd35535bb655cc
xufanghui <xufanghui@yeecall.com>
2021-05-09 20:31:20 +0800

    reset build html

--- docs/_next/static/development/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST = {__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/_error":["static\u002Fchunks\u002Fpages\u002F_error.js"],"/go.mod":["static\u002Fchunks\u002Fpages\u002Fgo.mod.js"],sortedPages:["\u002F_app","\u002F_error","\u002Fgo.mod"]};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
